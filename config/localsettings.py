b3a460a2beef1136128f59d5f2c05812da002f84
hi1d <aopd408@gmail.com>
2022-03-08 13:16:45 +0900

    [Init]

---
+++ config/localsettings.py
@@ -0,0 +1,13 @@
+SECRET_KEY = 'django-insecure-2*0p)eiz5k1vq%o0eqoxzw1#o_kw8gn-8you-#)ytmca^n99bj'
+DATABASES = {
+    'default': {
+        # 'ATOMIC_REQUESTS': True,   
+        'ENGINE': "django.db.backends.mysql",
+        'NAME': "DB NAME",
+        "USER": "ID",
+        "PASSWORD": "PASSWORD",
+        "HOST": "RDS",
+        "PORT": "3306",
+    }
+}
+
