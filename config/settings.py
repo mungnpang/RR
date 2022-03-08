d056ce87aff05e9f84d567eca35dbdee3409e202
hi1d <aopd408@gmail.com>
2022-03-08 13:20:31 +0900

    [Init]

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from config.localsettings import *
+from config.local_settings import *
 import pymysql, os, json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
